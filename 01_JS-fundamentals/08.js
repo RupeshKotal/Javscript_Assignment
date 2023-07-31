1fd70b2f067e38b67a8cbe27e9d86cb48368b50b
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-07-31 14:01:34 +0000

    from assignment 8 to 10

---
+++ 01_JS-fundamentals/08.js
@@ -0,0 +1,42 @@
+// Assignment 08 
+/*
+let num =0;
+
+for(let i = 10; i >= num; i--){
+   // console.log(i)
+}
+*/
+
+// Assignment 10 :- Finding Positive number from arry
+
+let arr = [1,-2,3,-4,5,-6,-7,7]
+
+for(let i =0; i<arr.length; i++){
+    if(arr[i] > 0){
+       // console.log(arr[i])
+    }
+}
+
+// Assignment 09 :-
+/*
+const newArry= [1,2,3,"Rupesh",4 , "Pwskills", 8]
+const stringName = String(newArry)
+console.log(stringName)
+
+for(let i = 0; i<newArry.length; i++){
+//     if(typeof newArry[i] == stringName){
+//         console.log(newArry[i])
+//     }
+//     else(
+//         console.log("not found a string")
+//     )
+// }
+  function find(newArry)(
+
+  )
+
+}
+
+*/
+
+
