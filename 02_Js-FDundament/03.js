64a07312de73418bbf3ce68607c1c5ad31044365
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-17 18:35:44 +0000

    wrong have to solve again

---
+++ 02_Js-FDundament/03.js
@@ -0,0 +1,29 @@
+const student = [
+    {name:"Rupesh",
+    marks: 90},
+
+    {name:"Ritesh",
+    marks: 40},
+
+    {name:"Parth",
+    marks: 60},
+
+    {name:"Utkasrh",
+    marks: 75},
+]
+
+// console.log(name)
+function checkResult(std){
+    if(student[keys] == std){
+        if(student[marks] > 90){
+            console.log(`${std} is passed in exam`)
+        }else{
+            console.log(`${std} is faild in exam`)
+        }
+    }
+    else{
+        console.log(`${std} is invalid Student name`)
+    }
+}
+
+checkResult("Ritesh")
