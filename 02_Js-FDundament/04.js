a132f910b9b151110ba9332dea1732b93619a44d
RupeshKotal <127592131+RupeshKotal@users.noreply.github.com>
2023-08-18 18:26:56 +0000

    have to modify

---
+++ 02_Js-FDundament/04.js
@@ -0,0 +1,9 @@
+const products = [
+    { name: "Laptop", price: 120000 },
+    { name: "Mobile", price: 70000 },
+    { name: "Laptop Bag", price: 1120000 },
+    { name: "Watch", price: 20000 },
+    { name: "Mobile Charger", price: 1500 },
+  ];
+
+ let maxPrice = {name:"", pr}
